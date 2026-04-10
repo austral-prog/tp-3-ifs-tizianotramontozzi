8618d9215f9d6e5a0d77c1cdb362c7a27e51eacc
tizianotramontozzi <ttramontozzi@mail.austral.edu.ar>
2026-04-10 11:33:32 -0300

    Add files via upload

--- exercise_compare.py
+++ exercise_compare.py
@@ -15,7 +15,7 @@ def compare():
         Para las entradas "7" y "7", la salida esperada es:
         7 es igual a 7
     """
-    pass
+
     num1 = int(input())
     num2 = int(input())
 
@@ -25,5 +25,3 @@ def compare():
         print(f"{num1} es menor que {num2}")
     else:
         print(f"{num1} es igual a {num2}")
-
-compare()
